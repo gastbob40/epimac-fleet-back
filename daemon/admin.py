9ec3a8e00e28761b441555dc6dceab4ea89c3a36
Quentin Briolant <quentin.briolant@epita.fr>
2022-11-07 20:00:03 +0100

    feat(daemon): add category in admin page

--- daemon/admin.py
+++ daemon/admin.py
@@ -7,5 +7,20 @@ from daemon.models import IMacModel
 
 @admin.register(IMacModel)
 class IMacModelAdmin(admin.ModelAdmin):
-    list_display = ('label', 'ip', 'macos_version', 'memory', 'cpu_cores', 'last_seen',)
+    list_display = ('label', 'ip', 'macos_version', 'memory', 'cpu_cores', 'last_seen')
+
+    fieldsets = (
+        ('Config', {
+            'fields': ('label', 'ip', 'mac_user')
+        }),
+        ('Software properties', {
+           'fields': ('macos_version', 'macos_build_version')
+        }),
+        ('Hardware properties', {
+            'fields': ('storage_capacity', 'memory', 'cpu_cores',)
+        }),
+        ('Daemon', {
+            'fields': ('last_seen', 'status')
+        }),
+    )
     # Override the name on the admin page
